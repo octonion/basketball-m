1dc145e1543bfea11c5539d2579b1794cf03bee2
Christopher D. Long <galizur@gmail.com>
2014-11-18 11:50:01 -0500

    Height scripts.

--- ncaa_pbp/sql/avg_roster_height.sql
+++ ncaa_pbp/sql/avg_roster_height.sql
@@ -4,6 +4,6 @@ avg(split_part(height,'-',1)::float*12+split_part(height,'-',2)::float)::numeric
 from ncaa_pbp.team_rosters
 where not(height='-')
 and not(height like '4-%')
-and not(height 
+--and not(height 
 group by team_name
 order by avg_height desc;
