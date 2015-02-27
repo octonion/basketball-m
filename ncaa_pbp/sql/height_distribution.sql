a8d5acd18c71b80b6fc28d5618b2af6fa4b9b8de
Christopher D. Long <galizur@gmail.com>
2015-02-27 16:33:44 -0500

    NCAA play-by-play update for 2014-15.

--- ncaa_pbp/sql/height_distribution.sql
+++ ncaa_pbp/sql/height_distribution.sql
@@ -1,1 +1,8 @@
-select split_part(height,'-',1)::integer*12+split_part(height,'-',2)::integer as height,count(*) from ncaa_pbp.team_rosters where not(height='-') and not(height like '4%') and not(height='5-1') group by height order by height;
+select
+split_part(height,'-',1)::integer*12+split_part(height,'-',2)::integer as height,
+count(*) from ncaa_pbp.team_rosters
+where
+    not(height='-')
+and not(height like '4%')
+and not(height='5-1')
+group by height order by height;
