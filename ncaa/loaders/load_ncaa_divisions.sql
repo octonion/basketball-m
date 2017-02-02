6a7837ff6e275667077fc55e87de730edd638764
Christopher D. Long <galizur@gmail.com>
2017-02-02 01:23:44 -0500

    NCAA men's basketball update for 2017.

--- ncaa/loaders/load_ncaa_divisions.sql
+++ ncaa/loaders/load_ncaa_divisions.sql
@@ -16,15 +16,15 @@ create table ncaa.schools_divisions (
 
 copy ncaa.schools_divisions from '/tmp/ncaa_divisions.csv' with delimiter as ',' csv quote as '"';
 
--- Temporary fix for 2016
+-- Temporary fix for 2017
 
 insert into ncaa.schools_divisions
 (sport_code,school_name,school_id,pulled_name,javascript,year,div_id,school_year,sport,division)
 (
-select sport_code,school_name,school_id,pulled_name,javascript,2016,div_id,school_year,sport,division
+select sport_code,school_name,school_id,pulled_name,javascript,2017,div_id,school_year,sport,division
 from ncaa.schools_divisions
-where year=2015
-and (school_id,2016) not in
+where year=2016
+and (school_id,2017) not in
 (select school_id,year from ncaa.schools_divisions)
 );
 
